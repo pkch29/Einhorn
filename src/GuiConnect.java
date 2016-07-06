b57a5942978bb835f31d48d6a115f3e1921b44bd
Lisa Ehrlich <lisa.ehrlich@kooperation-jlu.fh-giessen.de>
2016-07-06 11:04:52 +0200

    Interface zum Verbinden der GUI mit dem Rest

---
+++ src/GuiConnect.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+/**
+ * Creates connection between GUI and Game classes
+ */
+public interface GuiConnect {
+    public void goStraight();
+    public void goLeft();
+    public void goRight();
+    public void goBack();
+    public boolean hasStraight();
+    public boolean hasLeft();
+    public boolean hasRight();
+    public void newGame();
+    public int[] getStats();
+    public String getHelp();
+    public String getRoom();
+    public List<String> showAndWait();
+    public void setName(String name);
+
+}
