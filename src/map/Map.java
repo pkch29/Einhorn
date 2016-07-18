3f38a9d639f40303a9503dded771ec1471f079f3
Peter Koch <peter.koch@kooperation-jlu.fh-giessen.de>
2016-07-18 17:58:00 +0200

    basic implementation of the interface.
    a torus like topology is implemented (leave left -> enter right, ...)
    returns the rooms image file and description
    player can move through the rooms.

--- src/map/Map.java
+++ src/map/Map.java
@@ -9,44 +9,65 @@ import java.util.List;
  */
 public class Map implements gui.GuiConnect {
 
-    private List<RoomInfo> rooms;
-    int currentRoomId;
+    private ArrayList<RoomInfo> rooms;
+    private int currentRoomId;
     //private Player player;
 
 
     @Override
     public void goBack() {
-
+        int playerOrientation = 0; //player.getOrientation();
+        go(playerOrientation+2);
     }
 
     @Override
     public void goStraight() {
-
+        int playerOrientation = 0; //player.getOrientation();
+        go(playerOrientation);
     }
 
     @Override
     public void goLeft() {
-
+        int playerOrientation = 0; //player.getOrientation();
+        go(playerOrientation-1);
     }
 
     @Override
     public void goRight() {
+        int playerOrientation = 0; //player.getOrientation();
+        go(playerOrientation+1);
+    }
 
+    private void go(int direction) {
+        //@TODO user always has to make sure that the direction exists!
+        // currently here is an additional check, but as there is no feedback given,
+        // its quite useless.
+        if (!rooms.get(currentRoomId).hasDoor(direction)) {
+            return;
+        }
+
+        //@TODO maybe some checks if we can leave?
+        // maybe we can only go backwards if there is still a creature in the room
+
+        currentRoomId = rooms.get(currentRoomId).getConnectedRoomId(direction);
     }
 
     @Override
     public boolean hasStraight() {
-        return false;
+        int playerOrientation = 0; //player.getOrientation();
+        return rooms.get(currentRoomId).hasDoor(playerOrientation);
     }
 
     @Override
     public boolean hasLeft() {
-        return false;
+        int playerOrientation = 0; //player.getOrientation();
+        return rooms.get(currentRoomId).hasDoor(playerOrientation - 1);
     }
 
     @Override
     public boolean hasRight() {
-        return false;
+        int playerOrientation = 0; //player.getOrientation();
+        return rooms.get(currentRoomId).hasDoor(playerOrientation + 1);
     }
 
     @Override
@@ -61,13 +82,25 @@ public class Map implements gui.GuiConnect {
 
     @Override
     public String getHelp() {
+        return "This is probably a general help text.";
+    }
+
+    @Override
+    public String getRoom()
+    {
         return null;
     }
 
     @Override
-    public String getRoom() {
-        return null;
+    public String getRoomImageFileName() {
+        return rooms.get(currentRoomId).getRoom().getImageName();
     }
+
+    @Override
+    public String getRoomDescription() {
+        return rooms.get(currentRoomId).getRoom().getDescription();
+    }
+
 
     @Override
     public List<String> showAndWait() {
@@ -76,13 +109,21 @@ public class Map implements gui.GuiConnect {
 
     @Override
     public void setName(String name) {
-
+        // player.setName(name);
     }
-
 
     private void initGame() {
         rooms = new ArrayList<>();
         currentRoomId = 0;
+        generateTorus();
     }
+
+
+    private void generateTorus() {
+        Room room = new Room("room.jpg", "Der sich wiederholende Standard-Raum");
+        RoomInfo info = new RoomInfo(room, 0, 0, 0, 0);
+        rooms.add(info);
+    }
+
 
 }
