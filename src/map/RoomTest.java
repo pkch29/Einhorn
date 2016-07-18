bd419c91900f964d2229c861143eafed8d9a8074
Peter Koch <peter.koch@kooperation-jlu.fh-giessen.de>
2016-07-18 14:05:39 +0200

    Room class and test added. The room can spawn a given creature and store a given item. Looting is possible if there is no living creature.

---
+++ src/map/RoomTest.java
@@ -0,0 +1,84 @@
+package map;
+
+import creature.Creature;
+import item.Item;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Test for class Room
+ */
+public class RoomTest {
+
+    Room room;
+    Creature creature;
+    Item item;
+
+    String imageFileName = "imagefile.jpg";
+    String description = "Description";
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        // create room
+        room = new Room(imageFileName, description);
+
+        // create creature
+        String creatureName = "name";
+        String creatureSpecies = "species";
+        String creatureDescription = "description";
+        int creatureHp = 10;
+        int creatureLevel = 1;
+        Item creatureItem = null;
+        creature = new Creature(creatureName, creatureSpecies, creatureDescription,
+                creatureHp, creatureLevel, creatureItem);
+
+        // create item
+        item = new Item();
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void creation() throws Exception {
+        assertEquals(room.getImageName(), imageFileName);
+        assertEquals(room.getDescription(), description);
+        assertEquals(room.hasCreature(), false);
+        assertEquals(room.hasItem(), false);
+        // room can not be looted, as there is no item in it.
+        assertEquals(room.canBeLooted(), false);
+        assertEquals(room.lootRoom(), null);
+    }
+
+
+    @Test
+    public void creature() throws Exception {
+        room.spawnCreature(creature);
+        assertEquals(room.hasCreature(), true);
+    }
+
+
+    @Test
+    public void item() throws Exception {
+        room.storeItem(item);
+        assertEquals(room.hasItem(), true);
+    }
+
+    @Test
+    public void loot() throws Exception {
+        room.storeItem(item);
+        assertEquals(room.canBeLooted(), true); // item, but no creature
+        assertEquals(room.lootRoom(), item);
+
+        room.spawnCreature(creature);
+        assertEquals(room.canBeLooted(), false); // item and creature that is alive
+
+        //@TODO: kill creature and test for looting
+        //killCreature();
+        //assertEquals(room.canBeLooted(), true); // item and dead creature
+    }
+
+
+}
