0eb466d4e3b902350d54bfee52095bedf2ce2556
Anna Bauer <anna.bauer94@yahoo.de>
2016-08-01 13:22:23 +0200

    added Methodbodys for the setMethods

--- src/Creatures/Player.java
+++ src/Creatures/Player.java
@@ -10,6 +10,17 @@ public class Player implements Creatures{
     String name;
 
 
+    public Player(String name, int level, int Hp){
+        this.name = name;
+        this.level = level;
+        this.HP = Hp;
+    }
+
+    @Override
+    public String toString(){
+
+        return "You are called:  "+ name ;
+    }
     @Override
     public int getHP() {
         return HP;
@@ -21,12 +32,33 @@ public class Player implements Creatures{
     }
 
     @Override
-    public int attack() {
-        return 0;
+    public int attack(int attackdamage, int level, int dice) {
+        int damage;
+
+        damage = (attackdamage*level)+ dice;
+        //Schlagstärke = Angriffskraft der Waffe * eigenes Level + Wurf eines D20 Würfels
+
+        return damage;
     }
 
     @Override
     public String getName() {
         return name;
+    }
+
+    @Override
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    @Override
+    public void setHp(int hp) {
+        this.HP = hp;
+    }
+
+    @Override
+    public void setLevel(int level) {
+        this.level = level;
     }
 }
