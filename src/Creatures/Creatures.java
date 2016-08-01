cb6b98c38ad45fe120a273205b538e884faeda9f
Anna Bauer <anna.bauer94@yahoo.de>
2016-08-01 13:07:30 +0200

    Added Methods to the Creature Interface

--- src/Creatures/Creatures.java
+++ src/Creatures/Creatures.java
@@ -4,31 +4,55 @@ package Creatures;
  * Created by Anna on 11.07.2016.
  */
 public interface Creatures {
-//test
 
     /**
-     *
+     * Turns the name into a String.
+     * @return the name of the crature as a String.
+     */
+    String toString();
+
+    /**
+     * Gets the health points of the creature.
      * @return the actual health points from the creature
      */
     int getHP();
 
     /**
-     *
+     * Gets the level of the creatures.
      * @return the actual level from the creature
      */
     int getLevel();
 
     /**
-     *
-     * @return
+     * Gets the attack damage of the creature.
+     * @return the attackdamage of the creature.
      */
     int attack();
 
     /**
-     *
-     * @return
+     * Gets the name of the creature.
+     * @return the creatures name.
      */
     String getName();
+
+    /**
+     *
+     * @param name
+     */
+    void setName(String name);
+
+    /**
+     *
+     * @param hp
+     */
+    void setHp(int hp);
+
+    /**
+     *
+     * @param level
+     */
+    void setLevel(int level);
+
 
 
 }
