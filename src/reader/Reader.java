988ebd792b26fcb9c448742d30ab6065e555450b
Simone Langner <Simone Langner>
2016-09-10 12:04:29 +0200

    reader-methode

---
+++ src/reader/Reader.java
@@ -0,0 +1,34 @@
+
+package reader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Simone on 10.09.2016.
+ */
+public class Reader {
+
+    public List<String> read(String filename) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/Resources/"+filename)));
+        List<String> valueList = new ArrayList<>();
+        String line;
+        String[] values;
+        while((line = reader.readLine()) != null){
+            values = line.split(":");
+            if(values.length > 1){
+                valueList.add(values[1].trim());
+            }
+        }
+        reader.close();
+        return valueList;
+    }
+
+}
