57ab47240830a16f7a111592473f9d98869bebdf
Peter Koch <peter.koch@kooperation-jlu.fh-giessen.de>
2016-09-13 18:56:04 +0200

    removed public identifiers in interface

--- src/gui/GuiConnect.java
+++ src/gui/GuiConnect.java
@@ -11,83 +11,86 @@ public interface GuiConnect {
      * Tell player to move forward
      * Test with hasStraight() before using this method.
      */
-    public void goStraight();
+    void goStraight();
 
     /**
      * Tell player to move left
      * Test with hasLeft() before using this method.
      */
-    public void goLeft();
+    void goLeft();
 
     /**
      * Tell player to move right
      * Test with hasRight() before using this method.
      */
-    public void goRight();
+    void goRight();
 
     /**
      * Tell player to move backwards
      * Test with hasBack() before using this method.
      */
-    public void goBack();
+    void goBack();
 
     /**
      * Test if player can move forward
      */
-    public boolean hasStraight();
+    boolean hasStraight();
 
     /**
      * Test if player can move left
      */
-    public boolean hasLeft();
+    boolean hasLeft();
 
     /**
      * Test if player can move right
      */
-    public boolean hasRight();
+    boolean hasRight();
 
     /**
      * Test if player can move backwards
      */
-    public boolean hasBack();
+    boolean hasBack();
 
     /**
      * Start a new game
      */
-    public void newGame();
+    void newGame();
 
     /**
      * Get player stats, String[] mit Name an 0, KP an 1, Level an 2 und Name von Weapon an 3
      */
-    public String[] getStats();
+    String[] getStats();
 
     /**
      * Get help for current room
      */
-    public String getHelp();
+    String getHelp();
 
     /**
      * Returns the file name of the image for the current room.
      * @return the file name of the image for the current room
      */
-    public String getRoomImageFileName();
+    String getRoomImageFileName();
 
     /**
      * Returns the description of the current room.
      * @return the description of the current room
      */
-    public String getRoomDescription();
+    String getRoomDescription();
 
     /**
      * Returns the messages the GUI needs to display during a fight with a creature,
      * or finding of treasures.
      * @return A list of Strings which will be displayed
      */
-    public List<String> showAndWait();
+    List<String> showAndWait();
 
     /**
      * Set player name
      */
-    public void setPlayerName(String name);
-
+    void setPlayerName(String name);
 }
+
+
+
+
