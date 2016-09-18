948c2a46a2d991e4d534aaed16faae0104d8d3c4
Penelope Phlox <bachl.van@gmail.com>
2016-09-18 12:24:17 +0200

    methoden aus interface implementiert

--- src/messenger/GermanMessenger.java
+++ src/messenger/GermanMessenger.java
@@ -11,9 +11,7 @@ public class GermanMessenger implements Messenger {
 
     private List<String> messages = new ArrayList<>();
 
-    /**
-     * Player won the game
-     */
+    @Override
     public void playerWon() {
         messages.clear();
         messages.add("GAME OVER - Du hast gewonnen!");
@@ -22,6 +20,28 @@ public class GermanMessenger implements Messenger {
         messages.add("Du befindest Dich wieder am Eingang - aber ohne Schatz.");
     }
 
+    @Override
+    public void help(){
+
+    }
+
+    @Override
+    public void gameLost(String creatureName, String weaponNameCreature){
+
+    }
+
+    @Override
+    public void creatureDied(String creatureName, String weaponNamePlayer, int hpPlayer, int damage){
+
+    }
+
+    @Override
+    public void playerAttacked(String weaponNamePlayer, String weaponNameCreature, String creatureName,
+                               boolean hasPlayerWon, int hpPlayer, int hpCreature, int damage){
+
+    }
+
+    @Override
     public List<String> getMessages() {
         return messages;
     }
