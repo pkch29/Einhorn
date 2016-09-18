e7a96d903c0a26b1bd241a8b406c410d976dda94
Penelope Phlox <bachl.van@gmail.com>
2016-09-18 12:11:14 +0200

    methoden im interface hinzugefügt

--- src/messenger/Messenger.java
+++ src/messenger/Messenger.java
@@ -16,4 +16,39 @@ public interface Messenger {
      * @return messages of list
      */
     List<String> getMessages();
+
+    /**
+     * Player get help
+     */
+    void help();
+
+    /**
+     * Player lost the fight and died
+     * @param creatureName
+     * @param weaponName
+     */
+    public void gameLost(String creatureName, String weaponName);
+
+    /**
+     * Creature lost the fight and died
+     * @param creatureName
+     * @param weaponName
+     * @param hpPlayer
+     * @param damage
+     */
+    void creatureDied(String creatureName, String weaponName, int hpPlayer, int damage);
+
+    /**
+     * Message after fight, both are still alive
+     * @param weaponName
+     * @param creatureName
+     * @param hasPlayerWon
+     * @param hpPlayer
+     * @param hpCreature
+     * @param damage
+     */
+    void playerAttacked(String weaponName, String creatureName, boolean hasPlayerWon, int hpPlayer, int hpCreature,
+                        int damage);
+
+
 }
